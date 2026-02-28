a181aefcc4ac3154062ce92a84532c4717dce5ff
iamsahan <weerakkodyse@gmail.com>
2026-02-28 19:57:28 +0530

    feat: add Firebase messaging and notification support

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export { authService } from './api/AuthService';
 export { chatService } from './api/ChatService';
 export { apiClient } from './api/client';
+export { initializeFirebaseMessaging, registerBackgroundMessageHandler } from './firebaseMessaging';
 export type { AuthTokens, User } from '@types';
